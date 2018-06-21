6552325fd0f81185d9c1d448f6b6c6c3d1300932
Christina Wolfe <christina.wolfe@ibanity.com>
2018-06-21 10:06:53 +0200

    Improve argument descriptions

--- authorize.rb
+++ authorize.rb
@@ -12,23 +12,23 @@ when "account-information-access"
       puts parser
       exit
     end
-    parser.on("-f", "--financial-institution-id FINANCIAL_INSTITUTION_ID", "UUID for the associated financial institution") do |v|
+    parser.on("-f", "--financial-institution-id FINANCIAL_INSTITUTION_ID", "UUID of the financial institution holding the account to be authorized") do |v|
       options[:financial_institution_id] = v
     end
 
-    parser.on("-l", "--user-login USER_LOGIN", "User's fake financial institution login") do |v|
+    parser.on("-l", "--user-login USER_LOGIN", "Financial institutiton user's login") do |v|
       options[:user_login] = v
     end
 
-    parser.on("-p", "--user-password USER_PASSWORD", "User's fake financial institution password") do |v|
+    parser.on("-p", "--user-password USER_PASSWORD", "Financial institutiton user's password") do |v|
       options[:user_password] = v
     end
 
-    parser.on("-a", "--account-reference ACCOUNT_REFERENCE", "Financial institution's internal reference for the financial institution account to be authorized") do |v|
+    parser.on("-a", "--account-reference ACCOUNT_REFERENCE", "Reference value of the financial institution account to be authorized (NOT THE UUID)") do |v|
       options[:account_reference] = v
     end
 
-    parser.on("-r", "--account-information-access-request-redirect-link ACCOUNT_INFORMATION_ACCESS_REQUEST_REDIRECT_LINK", "URI returned when the account information access request is created") do |v|
+    parser.on("-r", "--account-information-access-request-redirect-link ACCOUNT_INFORMATION_ACCESS_REQUEST_REDIRECT_LINK", "Redirect URI from the created account information access request") do |v|
       options[:account_information_access_request_redirect_link] = v
     end
   end.parse!
